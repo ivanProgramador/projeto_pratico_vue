6bcb3149cb82586b92a09f815e9fe8e5097e1954
ivan <ivan-guita@hotmail.com>
2024-04-25 10:13:20 -0300

    final do curso de vue js

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -6,6 +6,8 @@
       <h4 class="is-size-4">Pokedex</h4>
 
       <input type="text" placeholder="Busca pelo nome" v-model="busca" class="input is-rounded" >
+
+      <!-- 1 esse botão tem o evento click para acionar a função de busca-->
       <button class="button is-normal is-success" id="buscaBtn" @click="buscar">Busca</button>
 
 
@@ -47,6 +49,9 @@ export default {
 
       console.log(res.data.results);
       this.pokemons = res.data.results;
+  
+      // 2 - um array vazio foi criado para guardar os pokemnos filtrados 
+
       this.filteredPokemons = res.data.results;
     
         
@@ -57,13 +62,25 @@ export default {
      PokemonComponent,
   },
   methods:{
-
+   
     buscar:function(){
-       
+       //a função de busca chama o array vazio e coloca todos os pokemons dentro dele 
+      
         this.filteredPokemons = this.pokemons;
+
+        // depois testa o campo de busca pra ver se o susario digitou alguma coisa 
+
         if(this.busca == '' || this.busca == ' '){
+          // se não digitou o array de pokemons filtrados recebe so os pokemnos que já estavam lá
+          // e continua exibindo todos 
+
             this.filteredPokemons = this.pokemons;
+
         }else{
+          // se o cliente tiver digitado alguma coisa no campo de busca 
+          // ele pega o array de pokemons faz um filtro com base na propriadade nome 
+          // e deixa somente o pokemon que o cliente buscou dentro array e exibe somente ele 
+
              this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
         }
     }
