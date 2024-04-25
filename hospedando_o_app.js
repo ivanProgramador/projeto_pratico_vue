ce7ceb43313849ab2a912bf6ad65db8cc4d6c04f
ivan <ivan-guita@hotmail.com>
2024-04-25 10:47:48 -0300

    projeto compilado e hospedado na unbler

---
+++ hospedando_o_app.js
@@ -0,0 +1,35 @@
+/*
+Eu usei a umbler apara fazer isso 
+1 - clicar em criar um site 
+2 - escrever qualquer nome de dominio(não
+    precisa ter exatamente um dominio para fazer isso 
+    na umbler ate porque é um app de teste)
+    eu inventei esse https://app.umbler.com/meuappdepokemon.com/website
+
+3 - depois esclher a opção site php(PHP e só nome do serviço a refrencia a essa linguagem e só nominal) 
+4 - clicar em concluir (ai o sistema vai começar a criar um site )
+5 - quando ele terminar clicar no botão ir para o site 
+
+6 - antes de subir o porjeto ele precisa ser compilado
+
+-----------------------------------------------------------------------------
+    acessar a pasta pokedex e digitar 'npm run build' depois que ele terminar 
+    ele vai colocar a ver~sao compilada do porjeto dentro de um pasta chamada dist 
+    ate aqui aqui ele so rodou em um servidor http de teste, agora aver~sao compilada
+    vai subir para um servidor http real
+-----------------------------------------------------------------------------------
+7 - clicar na opção aqruivos e depois na opção Deploy 
+8 - vai aparacer um local arrasta e solta para que eu possa colocar os arquivos 
+    do meu app então basta eu selecionar tudo e arrastar que ele vai apra dentro do servidor
+
+9 - depois de completar o upload e so cliencar no link 
+http://meuappdepokemon-com.umbler.net 
+que fica do lado do icone do php pronto agora a plicação vai rodar normalmente so que em um servidor
+externo.
+
+
+
+
+
+
+*/
